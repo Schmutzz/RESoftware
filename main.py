2683b4ec9f49dbe909a18338321438e3c3a81daa
Schmutzz <wengert.alex@web.de>
2021-10-27 23:33:35 +0200

    erste Datenbank versuche

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import sqlite3
 print("erste Zeile")
 
 x=7
@@ -13,11 +13,25 @@ z=9
 
 print("Boje ist ein kleiner Fiker")
 
-def validationString(exampleString,maxSize,csvObjekt):
-    if csvObjekt > maxSize:
-        return True
+verbindung = sqlite3.connect("C:\SQLiteStudio/"
+                             "production")
+zeiger = verbindung.cursor()
+sql_anweisung = """
+    CREATE TABLE produktion (
+    Date DATE,
+    Time TIME,
+    Cons_Prod VARCHAR(20),
+    Location  VARCHAR(50),
+    Energynetwork VARCHAR(50),
+    Power DOUBLE(8,2),
+    Energysource VARCHAR(50)
+    );"""
 
-    if csvObjekt != exampleString:
-        return False
+zeiger.execute(sql_anweisung)
 
-    return True
+verbindung.commit()
+verbindung.close()
+
+b=9
+
+print("Boje ist ein kleiner Fiker")
