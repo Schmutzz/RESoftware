41827eba86dfaaef43d4f28497ff0f248a02f072
Schmutzz <wengert.alex@web.de>
2021-11-23 17:43:09 +0100

    Stand 23.11.21 17:42 Mit Geo neu

--- geo.py
+++ geo.py
@@ -1,7 +1,8 @@
 from geopy.geocoders import get_geocoder_for_service
+from geopy import distance
 
 
-# getting OSM data for a specific location
+# getting OSM data for a single, specific location
 class SingleLocation:
 
     # locationSpecifier is giving a specific area to search the location in
@@ -43,7 +44,6 @@ class SingleLocation:
         else:
             return 'not_found'
 
-
     def getRaw(self):
         temp = self.geocode(str(self.location), str(self.locationSpecifier))
         if temp != False:
@@ -51,12 +51,19 @@ class SingleLocation:
         else:
             return 'not_found'
 
+    def getCoords(self):
+        temp = self.geocode(str(self.location), str(self.locationSpecifier))
+        if temp != False:
+            return [temp['lat'], temp['lon']]
+        else:
+            return 0.0
+
 
 # getting the mean coordinates of multiple given locations, also works for single locations
 class Multiple:
 
     # locationSpecifier is giving a specific area to search the location in
-    # locationList can be a single location
+    # locationList can also be a single location (postal code or string)
     def __init__(self, locationList, locationSpecifier):
         self.locationList = locationList
         self.locationSpecifier = locationSpecifier
@@ -81,24 +88,46 @@ class Multiple:
         y = 0.0
         counter = 0
 
-        # checking if the method is working with a list
+        # for lists
         if isinstance(self.locationList, list):
-            for i in self.locationList:
-                temp = self.geocode(i, self.locationSpecifier)
-                if temp != False:
-                    x += float(temp['lon'])
-                    y += float(temp['lat'])
-                    counter += 1
 
-            if counter != 0:
-                # calculating the mean values
-                x = x / counter
-                y = y / counter
+            # for list of strings
+            if all(isinstance(n, str) for n in self.locationList):
+                for i in self.locationList:
+                    temp = self.geocode(i, self.locationSpecifier)
+                    if temp != False:
+                        x += float(temp['lon'])
+                        y += float(temp['lat'])
+                        counter += 1
 
-                return [round(y, 7), round(x, 7)]
+                if counter != 0:
+                    # calculating the mean values
+                    x = x / counter
+                    y = y / counter
 
-            else:
-                return [0, 0]
+                    return [round(y, 7), round(x, 7)]
+
+                else:
+                    return [0, 0]
+
+            # for list of integers (postal codes)
+            elif all(isinstance(n, int) for n in self.locationList):
+                for i in self.locationList:
+                    temp = self.geocode(str(i), self.locationSpecifier)
+                    if temp != False:
+                        x += float(temp['lon'])
+                        y += float(temp['lat'])
+                        counter += 1
+
+                if counter != 0:
+                    # calculating the mean values
+                    x = x / counter
+                    y = y / counter
+
+                    return [round(y, 7), round(x, 7)]
+
+                else:
+                    return [0, 0]
 
         # returning data if only one location is given
         elif isinstance(self.locationList, str):
@@ -111,7 +140,7 @@ class Multiple:
             else:
                 return [0, 0]
 
-        # returning data if a postal code is given
+        # returning data if one postal code is given
         elif isinstance(self.locationList, int):
 
             temp = self.geocode(str(self.locationList), self.locationSpecifier)
@@ -128,16 +157,60 @@ class Multiple:
 
         # checking if the method is working with a list
         if isinstance(self.locationList, list):
-            for i in self.locationList:
-                x += float(self.geocode(i, self.locationSpecifier)['lon'])
-                counter += 1
 
-            # returning the mean
-            return x / counter
+            # for list of strings
+            if all(isinstance(n, str) for n in self.locationList):
+                for i in self.locationList:
+                    temp = self.geocode(i, self.locationSpecifier)
+                    if temp != False:
+                        x += float(temp['lon'])
+                        counter += 1
 
-        # returning data if only one location is given
-        elif isinstance(self.locationList, str):
-            return self.geocode(self.locationList, self.locationSpecifier)['lon']
+                if counter != 0:
+                    # calculating the mean values
+                    x = x / counter
+
+                    return round(x, 7)
+
+                else:
+                    return 0.0
+
+            # for list of integers (postal codes)
+            elif all(isinstance(n, int) for n in self.locationList):
+                for i in self.locationList:
+                    temp = self.geocode(str(i), self.locationSpecifier)
+                    if temp != False:
+                        x += float(temp['lon'])
+                        counter += 1
+
+                if counter != 0:
+                    # calculating the mean values
+                    x = x / counter
+
+                    return round(x, 7)
+
+                else:
+                    return 0.0
+
+            # returning data if only one location is given
+            elif isinstance(self.locationList, str):
+
+                temp = self.geocode(self.locationList, self.locationSpecifier)
+                if temp != False:
+                    x = float(temp['lon'])
+                    return round(x, 7)
+                else:
+                    return 0.0
+
+            # returning data if one postal code is given
+            elif isinstance(self.locationList, int):
+
+                temp = self.geocode(str(self.locationList), self.locationSpecifier)
+                if temp != False:
+                    x = float(temp['lon'])
+                    return round(x, 7)
+                else:
+                    return 0.0
 
     def getMeanY(self):
         y = 0.0
@@ -145,14 +218,237 @@ class Multiple:
 
         # checking if the method is working with a list
         if isinstance(self.locationList, list):
-            for i in self.locationList:
-                y += float(self.geocode(i, self.locationSpecifier)['lat'])
-                counter += 1
 
-            # returning the mean
-            return y / counter
+            # for list of strings
+            if all(isinstance(n, str) for n in self.locationList):
+                for i in self.locationList:
+                    temp = self.geocode(i, self.locationSpecifier)
+                    if temp != False:
+                        y += float(temp['lon'])
+                        counter += 1
 
-        # returning data if only one location is given
-        elif isinstance(self.locationList, str):
-            return self.geocode(self.locationList, self.locationSpecifier)['lat']
+                if counter != 0:
+                    # calculating the mean values
+                    y = y / counter
 
+                    return round(y, 7)
+
+                else:
+                    return 0.0
+
+            # for list of integers (postal codes)
+            elif all(isinstance(n, int) for n in self.locationList):
+                for i in self.locationList:
+                    temp = self.geocode(str(i), self.locationSpecifier)
+                    if temp != False:
+                        y += float(temp['lon'])
+                        counter += 1
+
+                if counter != 0:
+                    # calculating the mean values
+                    y = y / counter
+
+                    return round(y, 7)
+
+                else:
+                    return 0.0
+
+            # returning data if only one location is given
+            elif isinstance(self.locationList, str):
+
+                temp = self.geocode(self.locationList, self.locationSpecifier)
+                if temp != False:
+                    y = float(temp['lon'])
+                    return round(y, 7)
+                else:
+                    return 0.0
+
+            # returning data if one postal code is given
+            elif isinstance(self.locationList, int):
+
+                temp = self.geocode(str(self.locationList), self.locationSpecifier)
+                if temp != False:
+                    y = float(temp['lon'])
+                    return round(y, 7)
+                else:
+                    return 0.0
+
+
+# adds a coords column to a dataframe ('Coords')
+def addCoords(df, locationHeader, specifierHeader, finished_filename):
+
+    locationList = replaceSpecialSigns(df[locationHeader].tolist())
+    specifierList = replaceSpecialSigns(df[specifierHeader].tolist())
+    coords = []
+
+    for index, location in enumerate(locationList):
+        if locationList[index].contains(','):
+            locationList[index] = listFromStr(locationList[index], ',')
+
+    for index, location in enumerate(locationList):
+        coords.append(getCoords(location, specifierList[index]))
+
+    df['Coords'] = coords
+    df.to_csv(finished_filename, sep=';', index=False, encoding='latin1')
+
+
+# adds weatherstation-ID to a dataframe ('Wetter-ID_Head')
+def addWeather(df_locations, df_weatherStations, locationCoordsHead, stationCoordsHead, ID_Head, finished_filename):
+
+    ids = df_weatherStations[ID_Head].tolist()
+    weatherCoords = df_weatherStations[stationCoordsHead].tolist()
+    locationCoords = df_locations[locationCoordsHead].tolist()
+    id_list = []
+
+    for location in locationCoords:
+        min_id = 0
+        min_dist = 0
+
+        # distance method argument is a tuple
+        location = editCoords(location)
+        for index, station in enumerate(weatherCoords):
+            station = editCoords(station)
+            temp_dist = distance.distance(location, station).km
+            if (temp_dist < min_dist) or index == 0:
+                min_dist = temp_dist
+                min_id = ids[index]
+
+        id_list.append(min_id)
+
+    df_locations['Wetter-ID_Head'] = id_list
+    df_locations.to_csv(finished_filename, sep=';', index=False, encoding='latin1')
+
+
+# adds list of weatherstation-IDs and corresponding weighting factors ('Wetter-ID_Head', 'Gewichtung)
+def addWeatherRadian(df_locations, df_weatherStations, locationCoordsHead, stationCoordsHead, ID_Head, finished_filename,
+                     radian=5):
+
+    # lists method works with
+    ids = df_weatherStations[ID_Head].tolist()
+    weatherCoords = df_weatherStations[stationCoordsHead].tolist()
+    locationCoords = df_locations[locationCoordsHead].tolist()
+
+    # empty lists method will return
+    id_list = []
+    factor_list = []
+    radianReset = radian
+
+    for i, location in enumerate(locationCoords):
+        ids_in_radian = []
+        distances = []
+        weatherReset = []
+
+        # distance method argument is a tuple
+        location = editCoords(location)
+
+        while len(ids_in_radian) < 3:
+            for index, stationCoords in enumerate(weatherCoords):
+
+                stationCoords_temp = editCoords(stationCoords)
+
+                temp_dist = distance.distance(location, stationCoords_temp).km
+                if temp_dist < radian:
+                    ids_in_radian.append(ids[index])
+                    ids.remove(ids[index])
+                    weatherReset.append(stationCoords)
+                    weatherCoords.remove(stationCoords)
+                    distances.append(temp_dist)
+
+            radian += 3
+
+        # return used ids/coordinates to the complete list
+        for j in ids_in_radian:
+            ids.append(j)
+        for k in weatherReset:
+            weatherCoords.append(k)
+
+        radian = radianReset
+        factors = calcFactor(distances, radian)
+        factor_list.append(factors)
+        id_list.append(ids_in_radian)
+        print(i)
+
+    df_locations['Wetter-ID_Head'] = id_list
+    df_locations['Gewichtung'] = factor_list
+    df_locations.to_csv(finished_filename, sep=';', index=False, encoding='latin1')
+
+
+# combines two coordinates columns into one ('Coords')
+def combineCoords(df, longitude_head, latitude_head):
+
+    lon = df[longitude_head].tolist()
+    lat = df[latitude_head].tolist()
+    coords = []
+
+    for index, x in enumerate(lon):
+        coords.append(str([lon[index], lat[index]]))
+
+    df['Coords'] = coords
+
+
+# returns coords as tuple
+def editCoords(coords):
+
+    coords = str(coords)
+    coords = coords.replace('[', '')
+    coords = coords.replace(']', '')
+    coords = tuple(listFromStr(coords, ', '))
+    return coords
+
+
+# calculates "weight-factors"
+def calcFactor(distances, radian):
+
+    factors = []
+    scoreList = []
+    scoreSum = 0.0
+
+    for i, dist in enumerate(distances):
+        scoreList.append(1000 * (1 / (pow(2, dist / (radian * 1.5)))))
+        scoreSum += scoreList[i]
+
+    for score in scoreList:
+        factors.append(float.__round__(score / scoreSum, 6))
+
+    return factors
+
+
+# cut a string into a list of strings via certain seperation sign
+def listFromStr(text, seperator):
+
+    list_of_str = text.split(seperator)
+    return list_of_str
+
+
+# get mean coords for a list of locations
+def getCoords(list_of_cities, specifier):
+
+    object = Multiple(list_of_cities, specifier)
+
+    return object.getMeanCoords()
+
+
+# replaces encoded special signs (ä, ö, ü, ß)
+def replaceSpecialSigns(text):
+
+    if isinstance(text, list):
+        for index, i in enumerate(text):
+            if isinstance(i, str):
+                i = i.replace('Ã¼', 'ü')
+                i = i.replace('Ã¶', 'ö')
+                i = i.replace('Ã', 'ß')
+                i = i.replace('Ã¤', 'ä')
+                i = i.replace('Kr Hzgt ', 'Herzogtum')
+                i = i.replace('Kr ', '')
+                i = i.replace('St.', 'Sankt')
+                text[index] = i
+    elif isinstance(text, str):
+        text = text.replace('Ã¼', 'ü')
+        text = text.replace('Ã¶', 'ö')
+        text = text.replace('Ã', 'ß')
+        text = text.replace('Ã¤', 'ä')
+        text = text.replace('Kr Hzgt ', 'Herzogtum')
+        text = text.replace('Kr ', '')
+        text = text.replace('St.', 'Sankt')
+
+    return text
